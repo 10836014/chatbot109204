392b032729c6dad3c11e1f5ed6afe85898ed3088
ruby <ruby.1999.0220@gmail.com>
2020-11-01 00:13:58 +0800

    fix: 回傳更動欄位數量

--- data/addUserInfromation.php
+++ data/addUserInfromation.php
@@ -14,14 +14,34 @@
     $sql="UPDATE users SET birthday='$_POST[birthday]',  gender='$_POST[gender]'
     WHERE user_id='$_POST[user_id]'";
 
-    mysqli_query($con,$sql);
+    // $num=mysqli_num_rows($result); // 適用於select
+    // $num=mysqli_affected_rows($result); // INSERT、UPDATE、DELETE
+    // var_dump(mysqli_affected_rows(mysqli_query($con,$sql)));
 
-    if (!mysqli_query($con,$sql)){
+    $con -> query("UPDATE users SET birthday='$_POST[birthday]',  gender='$_POST[gender]'
+    WHERE user_id='$_POST[user_id]'");
+
+    // echo "Affected rows: " . $con -> affected_rows ;
+    $result = ($con -> affected_rows);
+    $colum = ($con -> affected_rows <= 0);
+    // var_dump($colum);
+    var_dump($result);
+
+    if ($result !== 0){
+    // if ($colum === true){
         //die 'Error: ' . mysqli_error($con);
-        echo json_encode(array('result' => '1', 'data' => '失敗', 'error' => mysqli_error($con)));
+        echo json_encode(array('result' => '0', 'data' => "成功，修改到的資料數量 :" . $con -> affected_rows));
+        echo ('Error: ' . mysqli_error($con));
+    }
+    else if ($result == 0){
+        //die 'Error: ' . mysqli_error($con);
+        echo json_encode(array('result' => '1', 
+        'data' => "失敗，修改到的資料數量為零 :" . $con -> affected_rows, 
+        'message 1'=>'可能是修改資料相同，沒變更到',
+        'message 2'=>'或是查無此id'));
         echo ('Error: ' . mysqli_error($con));
     }else{
-        echo json_encode(array('result' => '0', 'data' => '成功'));
+        echo json_encode(array('result' => '0', 'data' => "傳入使用者生日、性別成功"));
     }
     mysqli_close($con);
 ?>
