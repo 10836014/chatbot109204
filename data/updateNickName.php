e592d694c0c0f881e3177f6d51a6b6288477327c
ruby <ruby.1999.0220@gmail.com>
2020-11-07 13:10:15 +0800

    fix: 刪掉多的echo

--- data/updateNickName.php
+++ data/updateNickName.php
@@ -18,7 +18,6 @@
     if (!mysqli_query($con,$sql)){
         //die 'Error: ' . mysqli_error($con);
         echo json_encode(array('result' => '1', 'data' => '修改聊天室中本人的暱稱失敗', 'error' => mysqli_error($con)));
-        echo ('Error: ' . mysqli_error($con));
     }else{
         echo json_encode(array('result' => '0', 'data' => '修改聊天室中本人的暱稱成功'));
     }
