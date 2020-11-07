0cec712cfc52e1ba9842c8210395f0dbb0ae6d6e
ruby <ruby.1999.0220@gmail.com>
2020-11-07 21:04:06 +0800

    feat: 計算貼文按讚數量

--- object/post.php
+++ object/post.php
@@ -20,20 +20,18 @@ class Post{
 
     function readPost(){
     
-        // select all query
         // $query = "SELECT * FROM posts";
 
         $query="SELECT p.*, u.user_name, h.habbit_cat_name
         FROM posts AS p INNER JOIN users AS u  INNER JOIN habbit_cat AS h 
-        ON p.user_id = u.user_id AND p.habbit_id = h.habbit_id";
+        ON p.user_id = u.user_id AND p.habbit_id = h.habbit_id ";
 
         // prepare query statement
         $stmt = $this->conn->prepare($query);
-    
+
         // execute query
         $stmt->execute();
 
-    
         return $stmt;
     }
 
@@ -49,11 +47,34 @@ class Post{
 
         // prepare query statement
         $stmt = $this->conn->prepare($query);
+
+        // execute query
+        $stmt->execute();
+
+        return $stmt;
+    }
+
+    function countLikes(){
+    
+        // select all query
+        // $query = "SELECT * FROM posts";
+
+        $query="SELECT p.*, u.user_name, h.habbit_cat_name, count(l.post_id) AS likesNum
+        FROM posts AS p 
+        INNER JOIN users AS u  
+        INNER JOIN habbit_cat AS h 
+        INNER JOIN likes AS l
+        ON p.user_id = u.user_id 
+        AND p.habbit_id = h.habbit_id 
+        AND l.post_id = p.post_id
+        GROUP BY p.post_id ";
+
+        // prepare query statement
+        $stmt = $this->conn->prepare($query);
     
         // execute query
         $stmt->execute();
 
-    
         return $stmt;
     }
 }
