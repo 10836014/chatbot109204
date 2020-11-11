79a58399622bbceb95091dcda58e80b59a08f6b9
ruby <ruby.1999.0220@gmail.com>
2020-11-11 23:32:36 +0800

    fix: 按讚後回傳貼文讚數

--- data/dislikePost.php
+++ data/dislikePost.php
@@ -18,24 +18,31 @@
     // mysqli_query($con,$sql);
 
     if ($result !== 0){
-        // if ($colum === true){
-            //die 'Error: ' . mysqli_error($con);
-            echo json_encode(array('result' => '0',
-            'data' => "取消按讚成功，欄位更新數量 :" . $con -> affected_rows));
+    $sql = "SELECT count(l.post_id) AS likes FROM posts AS p INNER JOIN likes AS l ON l.post_id = p.post_id WHERE p.post_id='$_POST[post_id]'";
+        $result = mysqli_query($con, $sql);
+        $like = mysqli_fetch_all($result, MYSQLI_ASSOC);
+        $like = ($like[0]['likes']);
+        echo json_encode(array('result' => '0',
+        'data' => "取消按讚成功，欄位更新數量 :" . $con -> affected_rows,
+        'likes' => $like));
+    }
+    else if ($result == 0){
+        //die 'Error: ' . mysqli_error($con);
+        echo json_encode(array('result' => '1', 
+        'data' => "失敗，修改到的資料數量為零 :" . $con -> affected_rows, 
+        'message'=>'可能是沒有對這貼文按過讚，或是已收回'));
+    }else if (!mysqli_query($con,$sql)){
+        echo json_encode(array('result' => '1',
+        'data' => '取消按讚失敗',
+        'error' => mysqli_error($con)));
         }
-        else if ($result == 0){
-            //die 'Error: ' . mysqli_error($con);
-            echo json_encode(array('result' => '1', 
-            'data' => "失敗，修改到的資料數量為零 :" . $con -> affected_rows, 
-            'message'=>'可能是沒有對這貼文按過讚，或是已收回'));
-        }else if (!mysqli_query($con,$sql)){
-            echo json_encode(array('result' => '1',
-            'data' => '取消按讚失敗',
-            'error' => mysqli_error($con)));
-            }
-        else{
-            echo json_encode(array('result' => '0',
-            'data' => "取消按讚成功"));
-        }
+    else{
+        $sql = "SELECT count(l.post_id) AS likes FROM posts AS p INNER JOIN likes AS l ON l.post_id = p.post_id WHERE p.post_id='$_POST[post_id]'";
+        $result = mysqli_query($con, $sql);
+        $like = mysqli_fetch_all($result, MYSQLI_ASSOC);
+        $like = ($like[0]['likes']);
+
+        echo json_encode(array('result' => '0', 'data' => "取消按讚成功", 'likes' => $like));
+    }
     mysqli_close($con);
 ?>
--- data/likePost.php
+++ data/likePost.php
@@ -10,8 +10,7 @@
 
     $selected = mysqli_select_db($con, "chatbot");
 
-    $sql = "INSERT INTO likes (user_id, post_id)
-    VALUES ('$_POST[user_id]', '$_POST[post_id]')";
+    $sql = "INSERT INTO likes (user_id, post_id) VALUES ('$_POST[user_id]', '$_POST[post_id]');";
 
     // mysqli_query($con,$sql);
 
@@ -19,7 +18,14 @@
     //die 'Error: ' . mysqli_error($con);
         echo json_encode(array('result' => '1', 'data' => '按讚失敗', 'error' => mysqli_error($con)));
     }else{
-        echo json_encode(array('result' => '0', 'data' => '按讚成功', 'return like_id: ' => $con->insert_id));
+        $sql = "SELECT count(l.post_id) AS likes FROM posts AS p INNER JOIN likes AS l ON l.post_id = p.post_id WHERE p.post_id='$_POST[post_id]'";
+        $result = mysqli_query($con, $sql);
+        $like = mysqli_fetch_all($result, MYSQLI_ASSOC);
+        $like = ($like[0]['likes']);
+        // $like = (($like['likes']));
+
+        echo json_encode(array('result' => '0', 'data' => '按讚成功', 'likes' => $like));
     }
+    
     mysqli_close($con);
 ?>
