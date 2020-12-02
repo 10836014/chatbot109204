2422f74eb07c7414f61cb9a24cf827cde1f0ecd5
ruby <ruby.1999.0220@gmail.com>
2020-12-03 01:23:43 +0800

    fix: 修正return_notice

--- data/return_notice.php
+++ data/return_notice.php
@@ -8,15 +8,14 @@
     }
 
     // 取得劇本資料
-    // $script = "SELECT * FROM notice ORDER BY sequence ASC, id ASC LIMIT 1";
     $script = "SELECT * FROM notice ORDER BY RAND() LIMIT 1";
-    // 用來測試句型回傳對不對(直接指定type)
-    // $script = "SELECT * FROM notice WHERE type = 3 ORDER BY sequence ASC, id ASC LIMIT 1";
     if (!mysqli_query($con, $script)) {
         echo json_encode(array('result' => '1', 'data' => '查無此劇本資料表_notice', 'error' => mysqli_error($con)));
     } else {
+        // notice phase, type collecte
         $script_sql = mysqli_query($con, $script);
         $script_result = mysqli_fetch_all($script_sql, MYSQLI_ASSOC);
+        // var_dump($script_result);
         // 取得劇本回傳欄位
         $id =$script_result[0]['id'];
         $type =$script_result[0]['type'];
@@ -24,113 +23,98 @@
         $phase2 =$script_result[0]['phase2'];
         $created_at =$script_result[0]['created_at'];
         $updated_at =$script_result[0]['updated_at'];
+
+        // chatroom data collection
         $chatroom = "SELECT * FROM chatrooms where chatroom_id= '$_POST[chatroom_id]' ";
-        if (!mysqli_query($con, $chatroom)) {
-            echo json_encode(array('result' => '1', 'data' => '與聊天室資料表連接錯誤'));
+        $chatroom_sql = mysqli_query($con, $chatroom);
+        $chatroom_result = mysqli_fetch_all($chatroom_sql, MYSQLI_ASSOC);
+
+        if (count($chatroom_result) == 0) {
+            echo json_encode(array('result' => '1', 'data' => '查無此聊天室，請確認傳入id是否正確'));
         } else {
-            $chatroom_sql = mysqli_query($con, $chatroom);
-            $chatroom_result = mysqli_fetch_all($chatroom_sql, MYSQLI_ASSOC);
+            // var_dump($type, $phase1, $phase2);
+            // 傳入聊天室id，回傳使用者資料
+            $chatroom_id =$chatroom_result[0]['chatroom_id'];
+            $user_id =$chatroom_result[0]['user_id'];
+            $nick_name =$chatroom_result[0]['nick_name'];
+            $role_name =$chatroom_result[0]['role_name'];
+            $habbit_name =$chatroom_result[0]['habbit_name'];
+            $original_intention = $chatroom_result[0]['original_intention'];
+            $goodness = $chatroom_result[0]['goodness'];
+            $badness = $chatroom_result[0]['badness'];
+            $completion = $chatroom_result[0]['completion'];
 
-            if (count($chatroom_result) == 0) {
-                echo json_encode(array('result' => '1', 'data' => '查無此聊天室，請確認傳入id是否正確'));
-            } else {
-                // 傳入聊天室id，回傳使用者資料
-                $chatroom_id =$chatroom_result[0]['chatroom_id'];
-                $user_id =$chatroom_result[0]['user_id'];
-                $nick_name =$chatroom_result[0]['nick_name'];
-                $habbit_name =$chatroom_result[0]['habbit_name'];
-                $original_intention = $chatroom_result[0]['original_intention'];
-                $goodness = $chatroom_result[0]['goodness'];
-                $badness = $chatroom_result[0]['badness'];
-                $completion = $chatroom_result[0]['completion'];
-
-                if ($type == 1) {
+            switch ($type) {
+                case 1:
                     $answer = $nick_name . $phase1. $habbit_name . $phase2 ;
-                } elseif ($type == 2) {
+                    break;
+                case 2:
                     $answer = $phase1 . $habbit_name ;
-                } elseif ($type == 3) {
+                    break;
+                case 3:
                     $answer = $phase1 . $habbit_name. $phase2 ;
-                } elseif ($type == 4) {
+                    break;
+                case 4:
                     $answer = $nick_name . $phase1 . '!' ;
-                } elseif ($type == 5) {
+                    break;
+                case 5:
                     $answer = $phase1 ;
-                } elseif ($type == 6) {
-                    $answer = $phase1 . $nick_name . $phase2 .'!' ;
-                }
+                    break;
+                case 6:
+                    $answer = $phase1 . $role_name . $phase2 .'!' ;
+                    break;
+              }
+            // var_dump($answer, $type);  // 很棒
 
-                $rand_int = (random_int(1, 100));
-                $rand = $rand_int % 13;
-
-                if ($rand == 0) {
-                    echo json_encode([
+            $rand_int = (random_int(1, 100));
+            $rand = $rand_int % 8 ;
+            // $rand =1;
+            var_dump($rand);
+            if ($rand == 0) {
+                echo json_encode([
                         'answer' => '不要再發懶了，快快去' . $habbit_name .'有我陪著你的！',
                         'type' => $rand
                     ]);
-                } elseif ($rand == 1) {
-                    echo json_encode([
+            } elseif ($rand == 1) {
+                echo json_encode([
                         'answer' => $answer,
                         'type' => $rand
                     ]);
-                } elseif ($rand == 2) {
-                    echo json_encode([
-                        'answer' => $answer,
-                        'type' => $rand
-                    ]);
-                } elseif ($rand == 3) {
-                    echo json_encode([
-                        'answer' => $answer,
-                        'type' => $rand
-                    ]);
-                } elseif ($rand == 4) {
-                    echo json_encode([
-                        'answer' => $answer,
-                        'type' => $rand
-                    ]);
-                } elseif ($rand == 5) {
-                    echo json_encode([
-                        'answer' => $answer,
-                        'type' => $rand
-                    ]);
-                } elseif ($rand == 6) {
-                    echo json_encode([
-                        'answer' => $answer,
-                        'type' => $rand
-                    ]);
-                } elseif ($rand == 7) {
-                    echo json_encode([
+            } elseif ($rand == 2) {
+                echo json_encode([
                         'answer' => '別忘記你的' . $original_intention .'阿!',
                         'type' => $rand
                     ]);
-                } elseif ($rand == 8) {
-                    echo json_encode([
+            } elseif ($rand == 3) {
+                echo json_encode([
                         'answer' => '如果你乖乖的' . $habbit_name .'就可以' . $goodness. '哦',
                         'type' => $rand
                     ]);
-                } elseif ($rand == 9) {
-                    echo json_encode([
+            } elseif ($rand == 4) {
+                echo json_encode([
                         'answer' => '再不' . $habbit_name . '小心' . $badness,
                         'type' => $rand
                     ]);
-                } elseif ($rand == 10) {
-                    echo json_encode([
+            } elseif ($rand == 5) {
+                echo json_encode([
                         'answer' => '你一定可以的！已經累計養成' . $completion . '次好習慣囉',
                         'type' => $rand
                     ]);
-                } elseif ($rand == 11) {
-                    echo json_encode([
+            } elseif ($rand == 6) {
+                echo json_encode([
                         'answer' => $habbit_name . '對你來說很容易的吧！你一定可以的',
                         'type' => $rand
                     ]);
-                } elseif ($rand == 12) {
-                    echo json_encode([
+            } elseif ($rand == 7) {
+                echo json_encode([
                         'answer' => '再堅持一下，' . $goodness . '離你不遠了！',
                         'type' => $rand
                     ]);
-                } else {
-                    echo json_encode([
-                        'answer' => '查無此句型格式'
+            } else {
+                echo json_encode([
+                        'answer' => '加油加油',
+                        'type' => $rand
                     ]);
-                }
             }
         }
     }
